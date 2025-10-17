63d0d1d9bc70ef9948f092be3eb60945b3986bc8
saidul <said38383742@gmail.com>
2025-10-18 03:24:00 +0600

    2nd

--- src/Components/Box/BoxName.jsx
+++ src/Components/Box/BoxName.jsx
@@ -1,12 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Box.css'
-const BoxName = ({country}) => {
+const BoxName = ({country,handleClick}) => {
+  console.log(handleClick)
+  const[visited, setVisited]=useState(false);
+  const handleVisited=()=>{
+    setVisited(!visited);
+    handleClick(country);
+  }
   return (
     
     <div className='box'>
       <img src={country.flags.flags.png}></img>
       <p>Name: {country.name.common}</p>
       <p>Area: {country.area.area}</p>
+      <button onClick={handleVisited }>{visited? 'visited': 'not visited'}</button>
     </div>
     
   );
--- src/Components/Country/Countries.jsx
+++ src/Components/Country/Countries.jsx
@@ -1,17 +1,35 @@
-import React, { use } from 'react';
+import React, { use, useState } from 'react';
 import BoxName from '../Box/BoxName';
 
+const Countries = ({ countriesPromise }) => {
+  const[visitedCountries,setVisitedCountries]=useState([]);
+  const handleClick=(country)=>{
+    const newVisitedCountries=[...visitedCountries,country]
+    setVisitedCountries(newVisitedCountries);
+  }
 
-
-const Countries = ({countriesPromise}) => {
-  const countriesData=use(countriesPromise)
-  const countries=countriesData.countries;
+  const countriesData = use(countriesPromise)
+  const countries = countriesData.countries;
   return (
-    <div className='countries'>
+    <div>
       <h1>in the Countries: </h1>
+      <p>Total Country visited: {visitedCountries.length}</p>
+    <ol>
       {
-        countries.map(country=> <BoxName country={country}></BoxName>)
+        visitedCountries.map(country=> (<li>{country.name.common}</li>))
       }
+    </ol>
+      <div style={{
+      display: "grid",
+      gridTemplateColumns: "repeat(3, 1fr)",
+      gap: "20px"
+    }}>
+        {
+          countries.map(country => (<BoxName country={country}
+            handleClick={handleClick}
+          ></BoxName>))
+        }
+      </div>
     </div>
   );
 };
